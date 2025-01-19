02e7a192ed0b9af0fc7b97f6ef7d29e8e9748a37
Jun CHOI <cjonyou@naver.com>
2025-01-19 16:05:36 +0900

    modify

--- mysql_config.sh
+++ mysql_config.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt install -y rsync
 sudo mkdir -p /workspace/mysql
-sudo chown -R mysql:mysql /workspace/mysql
+sudo chown -R gitpod:gitpod /workspace/mysql
 sudo rsync -av /var/lib/mysql/ /workspace/mysql/
